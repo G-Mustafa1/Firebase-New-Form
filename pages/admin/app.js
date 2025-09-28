85bc694bfbbf6c8bc396cde6a1768f7ac3876191
Ghulam Mustafa <157616503+G-Mustafa1@users.noreply.github.com>
2025-09-29 02:11:48 +0500

    Update app.js

--- pages/admin/app.js
+++ pages/admin/app.js
@@ -1,13 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
-import { getAuth, onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 import {
    getFirestore,
   collection,
   getDocs,
   deleteDoc,
   doc,
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 const firebaseConfig = {
    apiKey: "AIzaSyDIFb-in4PXBn_vOzyrkEVBk7f_jWPpz-0",
    authDomain: "loginsingup-72ff1.firebaseapp.com",
@@ -147,3 +147,4 @@ async function fetchStudents() {
  
  fetchStudents();
  fetchResults();
+
